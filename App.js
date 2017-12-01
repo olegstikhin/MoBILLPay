1cfbbcaecda3fecd5ba80daf8b63531cd0f7dd9d
Oleg Stikhin <oleg.stikhin@aalto.fi>
2017-12-01 21:10:05 +0200

    improved UI

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, Button, View, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View, AsyncStorage, Keyboard } from 'react-native';
 import { StackNavigator, } from 'react-navigation';
 
 var parseString = require('xml2js').parseString;
@@ -24,7 +24,7 @@ function post(url, params) {
 }
 
 export class HomeScreen extends React.Component {
-  static navigationOptions = { title: 'MoBILLPay', };
+  static navigationOptions = { title: 'MoBILLPay', headerLeft: null };
 
   constructor(props){
     super(props);
@@ -37,17 +37,18 @@ export class HomeScreen extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.mainheader}>Välkommen till MoBILLPay!</Text>
-        <Text>Genom att använda den här applikationen kan du överföra pengar mellan kopieringskonton.</Text>
-        <Text>Börja med att skriva in din fullständiga BILL-kod (8 siffror):</Text>
+        <Text style={styles.text}>Genom att använda den här applikationen kan du överföra pengar mellan kopieringskonton.</Text>
+        <Text style={styles.h2}>Logga in </Text>
+        <Text style={styles.text}>Börja med att skriva in din fullständiga BILL-kod (8 siffror):</Text>
         <TextInput
-          style={{height: 40, borderWidth: 1, padding: 5, borderColor: 'navy'}}
+          style={styles.inputfield}
           keyboardType="numeric"
           secureTextEntry={true}
           maxLength={8}
           onChangeText={(idText) => this.setState({idText})}
         />
-        <Button title="Fortsätt" onPress={() => this.saveId(this.state.idText) } />
-        <Text>MoBILLPay 0.1.1 - 1 december 2017</Text>
+        <Button style={styles.mainbutton} title="Logga in" onPress={() => this.saveId(this.state.idText) } />
+        <Text style={styles.text}>MoBILLPay 0.1.1 - 1 december 2017</Text>
       </View>
     );
   }
@@ -79,7 +80,7 @@ export class HomeScreen extends React.Component {
 }
 
 export class PayScreen extends React.Component {
-  static navigationOptions = { title: 'Överför', };
+  static navigationOptions = { title: 'Kontrollpanel', headerLeft: null};
 
   constructor(props){
     super(props);
@@ -93,20 +94,24 @@ export class PayScreen extends React.Component {
     const { state } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text style={styles.mainheader}>Överför</Text>
-        <Text>Hej {state.params.userName}! Du har {state.params.balance} på ditt BILL-konto.</Text>
-        <Text>Till vem?</Text>
+        <Text style={styles.mainheader}>Ditt konto</Text>
+        <Text style={styles.text}>Hej {state.params.userName}!</Text>
+        <Text style={styles.text}>Saldo: {state.params.balance}.</Text>
+        <Text style={styles.mainheader}>Ny överföring</Text>
         <TextInput
-          style={{height: 40, borderWidth: 1, padding: 5, borderColor: 'navy'}}
+          style={styles.inputfield}
           keyboardType="numeric"
           maxLength={8}
+          placeholder="Mottagare"
+          placeholderTextColor="black"
           onChangeText={(receiverIdText) => this.setState({receiverIdText})}
         />
-        <Text>Hur mycket pengar?</Text>
         <TextInput
-          style={{height: 40, borderWidth: 1, padding: 5, borderColor: 'navy'}}
+          style={styles.inputfield}
           keyboardType="numeric"
           maxLength={8}
+          placeholder="Transaktionsbelopp"
+          placeholderTextColor="black"
           onChangeText={(amountText) => this.setState({amountText})}
         />
         <Button title="Betala" onPress={() => this.preparePayment(state.params.billId, this.state.receiverIdText, this.state.amountText.split(',').join('.')) } />
@@ -127,6 +132,7 @@ export class PayScreen extends React.Component {
               receiverId: result.HTML.BODY[0].BR[0].P[0].FORM[0].INPUT[0].INPUT[0].INPUT[0].$.VALUE,
               receiverName: result.HTML.BODY[0].I[0],
               amount: amount,
+              userRealName: result.HTML.BODY[0].TABLE[0].TR[4].TD[1],
               userName: result.HTML.BODY[0].TABLE[0].TR[5].TD[1],
               balance: result.HTML.BODY[0].BR[0].P[0].FORM[0].INPUT[0].INPUT[0].INPUT[0].INPUT[0].INPUT[0].$.VALUE,
             });
@@ -146,18 +152,25 @@ export class ConfirmScreen extends React.Component {
 
   render() {
     const { state } = this.props.navigation;
+
+    Keyboard.dismiss();
+
     return (
       <View style={styles.container}>
-        <Text style={styles.mainheader}>Obekräftad betalning</Text>
-        <Text>Mottagare: {state.params.receiverName} ({state.params.receiverId})</Text>
-        <Text>Belopp: {state.params.amount} €</Text>
-
-        <Button title="Bekräfta betalningen" onPress={() => this.pay(state.params.billId, state.params.receiverId, state.params.receiverName, state.params.amount, state.params.balance) } />
+        <Text style={styles.mainheader}>Obekräftad överföring</Text>
+        <View style={styles.paymentinfo}>
+          <Text style={styles.paymentminor}>Överföring från</Text>
+          <Text style={styles.paymentrecipient}>{state.params.userRealName}</Text>
+          <Text style={styles.paymentminor}>till</Text>
+          <Text style={styles.paymentrecipient}>{state.params.receiverName}</Text>
+          <Text style={styles.paymentamount}>{state.params.amount} €</Text>
+        </View>
+        <Button style={styles.mainbutton} title="Bekräfta betalningen" onPress={() => this.pay(state.params.billId, state.params.receiverId, state.params.receiverName, state.params.userRealName, state.params.amount, state.params.balance) } />
       </View>
     );
   }
 
-  pay(billId, receiverId, receiverName, amount, balance) {
+  pay(billId, receiverId, receiverName, userRealName, amount, balance) {
     const { navigate } = this.props.navigation;
     post("https://bill.teknolog.fi/config", "code="+billId+"&account="+receiverId+"&amount="+amount+"&balance="+balance+"&check=1337&secret=1337")
     .then((responseText) => {
@@ -167,6 +180,7 @@ export class ConfirmScreen extends React.Component {
             billId: billId,
             receiverId: receiverId,
             receiverName: receiverName,
+            userRealName: userRealName,
             amount: amount,
           });
         } else alert("Ett fel inträffade!")
@@ -180,14 +194,24 @@ export class ConfirmScreen extends React.Component {
 }
 
 export class ReceiptScreen extends React.Component {
-  static navigationOptions = { title: 'Kvitto', };
+  static navigationOptions = { title: 'Kvitto', headerLeft: null };
   render() {
-    const { state } = this.props.navigation;
+    const { state, navigate } = this.props.navigation;
+
     return (
       <View style={styles.container}>
-        <Text style={styles.mainheader}>Kvitto</Text>
-        <Text>Mottagare: {state.params.receiverName} ({state.params.receiverId})</Text>
-        <Text>Belopp: {state.params.amount} €</Text>
+        <Text style={styles.mainheader}>Bekräftelse</Text>
+        <View style={styles.paymentreceipt}>
+          <Text style={[styles.paymentminor, styles.wtext]}>Överföring från</Text>
+          <Text style={[styles.paymentrecipient, styles.wtext]}>{state.params.userRealName}</Text>
+          <Text style={[styles.paymentminor, styles.wtext]}>till</Text>
+          <Text style={[styles.paymentrecipient, styles.wtext]}>{state.params.receiverName}</Text>
+          <Text style={[styles.paymentamount, styles.wtext]}>{state.params.amount} €</Text>
+        </View>
+        <Button
+          onPress={() => navigate('Home', {})}
+          title="Återvänd"
+        />
       </View>
     );
   }
@@ -213,9 +237,59 @@ export default class Root extends React.Component {
 
 const styles = StyleSheet.create({
   mainheader: {
-    color: 'navy',
     fontWeight: 'bold',
-    fontSize: 30,
+    fontSize: 24,
+    marginTop: 10,
+  },
+  h2: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginTop: 10,
+  },
+  mainbutton: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  text: {
+    marginTop: 10,
+  },
+  wtext: {
+    color: "white",
+  },
+  inputfield: {
+    height: 40,
+    padding: 5,
+  },
+  paymentinfo: {
+    alignSelf: 'stretch',
+    flex: 1,
+    marginTop: 30,
+    marginBottom: 60,
+    padding: 20,
+    backgroundColor: "#f4aa42",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  paymentreceipt: {
+    alignSelf: 'stretch',
+    flex: 1,
+    marginTop: 30,
+    marginBottom: 60,
+    padding: 20,
+    backgroundColor: "#0a680e",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  paymentrecipient: {
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  paymentminor: {
+    fontSize: 18,
+  },
+  paymentamount: {
+    fontWeight: 'bold',
+    fontSize: 48,
   },
   container: {
     flex: 1,
